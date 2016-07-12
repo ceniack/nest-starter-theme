4ce50f26dfe584da6a7ae2ec2e4cda2875d8d2f7
Terry Hale <thale@bigwing.com>
2016-07-12 12:23:08 -0500

    Added Activation for ACF Pro during theme install
    Signed-off-by: Terry Hale <thale@bigwing.com>

--- assets/utilities/utility-functions.php
+++ assets/utilities/utility-functions.php
@@ -158,3 +158,21 @@ function nest_custom_title( $post_id = 0, $before = '', $after = '', $echo = tru
 	    return $title;
     }
 }
+
+// Set ACF 5 license key on theme activation.
+function auto_set_license_keys() {
+  
+  if ( ! get_option( 'acf_pro_license' ) && defined( 'ACF_5_KEY' ) ) {
+    
+    $save = array(
+        'key'   => ACF_5_KEY,
+        'url'   => home_url()
+    );
+    
+    $save = maybe_serialize($save);
+    $save = base64_encode($save);
+      
+    update_option( 'acf_pro_license', $save );
+  }
+}
+add_action( 'after_switch_theme', 'auto_set_license_keys' );
